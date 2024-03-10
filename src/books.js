5b55b5b49c57040f3fb52b2a1030f690fdbfb245
ibrahima diallo <lodiajames@hotmail.com>
2024-03-10 18:22:03 -0400

    serve to github

---
+++ src/books.js
@@ -0,0 +1,29 @@
+
+import img1 from './images/book_1.jpg'
+import img2 from './images/book_2.jpg'
+import img3 from './images/book_3.jpg'
+
+
+
+
+export const books = [
+{
+ author: 'Jerry Izenberg',
+ title :'Once There Were Giants',
+img : img1,
+id: 1
+},
+{
+ author: 'James Clear',
+ title :'Atomic Habits',
+img : img2,
+id: 2
+
+},
+{
+ author: 'Mike Tyson',
+ title :'Undisputed Truth',
+img : img3,
+id:3
+}
+]
